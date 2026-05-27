a8e302f7b8fb18ce7dd7634d4d58d1ebef99880f
anand2257 <anand6780hyd@gmail.com>
2026-05-27 13:40:18 +0530

    fix: add exit hook to vite and set node engine

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'force-exit',
+      closeBundle() {
+        process.exit(0);
+      },
+    },
+  ],
+});
